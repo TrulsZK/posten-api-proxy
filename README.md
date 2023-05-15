cbd03615f20093f72aac1ee9f409c7f708cfc639
Truls Zhong Konstali <44983245+TrulsZK@users.noreply.github.com>
2023-05-15 20:49:11 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# posten-api-proxy
-Unofficial API Proxy script to get next mail delivery from Posten as JSON
+# Posten API Proxy
+
+Unofficial API Proxy script to get next mail delivery from Posten as JSON.
+
+This API can be hosted on a webserver and allow you to use the data from Posten in other scripts/services, e.g. Home Assistant RESTful sensor.
+
+This API uses the same (unofficial) API as the official tool at https://www.posten.no/levering-av-post.
+
+Since the URL and API key changes frequently this script will get the URL and API key from the official site and then query the API using the details extracted and output the data.
+
+## Usage
+
+To use the script send a GET request to the php script with your `postcode` as payload. You will then get a JSON back with the following response. E.g. https://127.0.0.1/posten.php?postcode=0001
+
+- `postentoday` `1` If mail will be delivered today (otherwise `0`)
+- `delivery0` Next delivery date (index 0) in ISO 8601 format (e.g. 2023-01-01)
+- `delivery1` Next delivery date after delivery0 (index 1) in ISO 8601 format (e.g. 2023-01-01)
+- `delivery2` Next delivery date after delivery1 (index 2) in ISO 8601 format (e.g. 2023-01-01)
+- `delivery3` Next delivery date after delivery2 (index 3) in ISO 8601 format (e.g. 2023-01-01)
+- `delivery4` Next delivery date after delivery3 (index 4) in ISO 8601 format (e.g. 2023-01-01)
+- `delivery0_formatted` Next delivery date (index 0) in `D j M Y` format (e.g. 1 Jan 2023)
+- `delivery1_formatted` Next delivery date after delivery0 (index 1) in `D j M Y` format (e.g. 1 Jan 2023)
+- `delivery2_formatted` Next delivery date after delivery1 (index 2) in `D j M Y` format (e.g. 1 Jan 2023)
+- `delivery3_formatted` Next delivery date after delivery2 (index 3) in `D j M Y` format (e.g. 1 Jan 2023)
+- `delivery4_formatted` Next delivery date after delivery3 (index 4) in `D j M Y` format (e.g. 1 Jan 2023)
+
+Example response:
+
+```JSON
+{
+  "postentoday": 0,
+  "delivery0": "2023-05-15",
+  "delivery1": "2023-05-16",
+  "delivery2": "2023-05-19",
+  "delivery3": "2023-05-22",
+  "delivery4": "2023-05-23",
+  "delivery0_formatted": "Mon 15 May 2023",
+  "delivery1_formatted": "Tue 16 May 2023",
+  "delivery2_formatted": "Fri 19 May 2023",
+  "delivery3_formatted": "Mon 22 May 2023",
+  "delivery4_formatted": "Tue 23 May 2023"
+}
+```
---
+++ posten.php
@@ -0,0 +1,114 @@
+<?php
+
+$postcodein = $_GET["postcode"];
+$postcode = filter_var($postcodein, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+
+function formatDate($dateString) {
+    $dateObject = new DateTime($dateString);
+    return $dateObject->format('D j M Y');
+}
+
+function checkPosten($postcode) {
+
+    // Headers
+    $headers0 = [
+        "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    ];
+
+    // Start Session
+    $session = curl_init();
+
+    // Load Posten Website to get URL and API token
+    $start_url = "https://www.posten.no/levering-av-post";
+    $start_payload = [];
+
+    curl_setopt($session, CURLOPT_URL, $start_url);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($session, CURLOPT_HTTPHEADER, $headers0);
+    $start_response = curl_exec($session);
+
+    $httpCode = curl_getinfo($session, CURLINFO_HTTP_CODE);
+    // Check if the response code is 200
+    if ($httpCode == 200) {
+        //echo "Response code is 200. Success!";
+    } else {
+        echo "Error: " . $httpCode . " when attempting to connect to " . $start_url;
+        exit;
+    }
+
+    preg_match('/"serviceUrl":"([^"]*)"/', $start_response, $serviceUrlMatches);
+    $serviceUrl = $serviceUrlMatches[1];
+
+    preg_match('/"apiKey":"([^"]*)"/', $start_response, $apiKeyMatches);
+    $apiKey = $apiKeyMatches[1];
+
+    // Check post code and delivery days
+    $headers1 = [
+        "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        "kp-api-token: " . $apiKey
+    ];
+
+    $checkpostcode_url = $serviceUrl . "?postalCode=" . $postcode;
+    $checkpostcode_payload = [];
+
+    curl_setopt($session, CURLOPT_URL, $checkpostcode_url);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($session, CURLOPT_HTTPHEADER, $headers1);
+    $checkpostcode_response = curl_exec($session);
+
+    $httpCode = curl_getinfo($session, CURLINFO_HTTP_CODE);
+    // Check if the response code is 200
+    if ($httpCode == 200) {
+        //echo "Response code is 200. Success!";
+    } else {
+        echo "Error: " . $httpCode . " when attempting to connect to " . $checkpostcode_url;
+        exit;
+    }
+    
+    $deliveryjson = json_decode($checkpostcode_response, true);
+    $delivery0 = $deliveryjson["delivery_dates"][0];
+    $delivery1 = $deliveryjson["delivery_dates"][1];
+    $delivery2 = $deliveryjson["delivery_dates"][2];
+    $delivery3 = $deliveryjson["delivery_dates"][3];
+    $delivery4 = $deliveryjson["delivery_dates"][4];
+
+    # Format Dates
+    $delivery0_formatted = formatDate($delivery0);
+    $delivery1_formatted = formatDate($delivery1);
+    $delivery2_formatted = formatDate($delivery2);
+    $delivery3_formatted = formatDate($delivery3);
+    $delivery4_formatted = formatDate($delivery4);
+
+    $postentoday = 0;
+    $today = date("Y-m-d");
+    if ($delivery0 === $today) {
+        $postentoday = 1;
+    }
+
+    return [$postentoday, $delivery0, $delivery1, $delivery2, $delivery3, $delivery4, $delivery0_formatted, $delivery1_formatted, $delivery2_formatted, $delivery3_formatted, $delivery4_formatted];
+}
+
+?>
+
+<?php
+
+list($postentoday, $delivery0, $delivery1, $delivery2, $delivery3, $delivery4, $delivery0_formatted, $delivery1_formatted, $delivery2_formatted, $delivery3_formatted, $delivery4_formatted) = checkPosten($postcode);
+
+// Create an associative array with the results
+$result = [
+    "postentoday" => $postentoday,
+    "delivery0" => $delivery0,
+    "delivery1" => $delivery1,
+    "delivery2" => $delivery2,
+    "delivery3" => $delivery3,
+    "delivery4" => $delivery4,
+    "delivery0_formatted" => $delivery0_formatted,
+    "delivery1_formatted" => $delivery1_formatted,
+    "delivery2_formatted" => $delivery2_formatted,
+    "delivery3_formatted" => $delivery3_formatted,
+    "delivery4_formatted" => $delivery4_formatted
+];
+
+echo json_encode($result, JSON_PRETTY_PRINT);
+
+?>
